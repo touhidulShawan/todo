5ff8f43827a9b689bf2ee2fcdc1314570912333a
Touhidul Shawan <touhidulshawan@gmail.com>
2019-08-10 15:15:44 +0600

    Updates

---
+++ precache-manifest.20d48caae5608b20bee5de3cee8a850e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a361c5510d727e222a8b241f9e2399eb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a9eb37d47291101ea1b2",
+    "url": "/todo/static/css/main.f6d49ec9.chunk.css"
+  },
+  {
+    "revision": "d380695811f1cb501751",
+    "url": "/todo/static/js/2.3e3c58b3.chunk.js"
+  },
+  {
+    "revision": "a9eb37d47291101ea1b2",
+    "url": "/todo/static/js/main.5014627e.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
